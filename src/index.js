fdc811d7494d8f0efc15a8e4fa5731a76b11693a
G1-H <hwangjiwon1992@gmail.com>
2025-01-11 02:10:05 +0900

    state & life cycle 학습

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 import Library from "./chapter_3/Library";
 import Clock from "./chapter_4/Clock";
 import CommentList from "./chapter_5/CommentList";
+import NotificationList from "./chapter_6/NotificationList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
