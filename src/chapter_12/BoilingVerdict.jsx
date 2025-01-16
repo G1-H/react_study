596058da0d698a0afd4c45fbfd6615ac0db94025
G1-H <hwangjiwon1992@gmail.com>
2025-01-16 18:28:25 +0900

    lifting state up 예제 수정

--- src/chapter_12/BoilingVerdict.jsx
+++ src/chapter_12/BoilingVerdict.jsx
@@ -1,6 +1,6 @@
 const BoilingVerdict = (props) => {
   if (props.temperature >= 100) {
-    return <p>물이 끓습니다.</p>;
+    return <p>{props.temperature} ℃ 로 100 ℃가 넘어 물이 끓습니다.</p>;
   }
   return <p>물이 끓지 않습니다.</p>;
 };
